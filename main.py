9e46dba72fb83b3e4426335b35634bba97296ec2
GYANENDRA8080 <gyanendra8080@gmail.com>
2025-07-14 16:17:12 +0530

    range

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-for i in range(1,11,2):
-    print(i)
+# a = "hello world"
+# for i in a:
+#     print(i)
+
+
+# a = "hello world"
+# for i in a:
+#     print(i)
+
+a = "hello world"
+for i in range(0,6,1):
+    print(a[i])    
+
+# a = "hello world"
+# for i in range(0,11,1):
+#     print(i)    
+
+# a = "hello world"
+# for i in range(0,12,1):
+#     print(a[i])
+
+# # a ="hello"
+# for i in range(0,5,1):
+#     print(i)
